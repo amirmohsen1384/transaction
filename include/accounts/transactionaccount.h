6d7e74d9ced83e588393b870ff9e0af150a645d2
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-17 17:59:07 +0330

    Implemented transaction account
    This class implements transaction account.

---
+++ include/accounts/transactionaccount.h
@@ -0,0 +1,18 @@
+#ifndef TRANSACTIONACCOUNT_H
+#define TRANSACTIONACCOUNT_H
+
+#include "abstractaccount.h"
+
+class TransactionAccount : public AbstractAccount
+{
+public:
+    TransactionAccount();
+
+public:
+    static TransactionAccount loadFromRecord(quint64 value);
+
+    virtual float profit() const override;
+    virtual void saveToRecord(quint64 value) const override;
+};
+
+#endif // TRANSACTIONACCOUNT_H
---
+++ src/accounts/transactionaccount.cpp
@@ -0,0 +1,74 @@
+#include "include/accounts/transactionaccount.h"
+
+constexpr auto suffix = "trf";
+
+TransactionAccount::TransactionAccount() {}
+
+float TransactionAccount::profit() const
+{
+    return 0 * this->balance;
+}
+
+TransactionAccount TransactionAccount::loadFromRecord(quint64 value)
+{
+    if(!Identifier::isValid(value, Account::Transaction))
+    {
+        qDebug() << QString("The given ID (%1) is not valid for a transaction account").arg(value);
+        return TransactionAccount();
+    }
+
+    const QString fileName = QString("%1.%2").arg(value).arg(suffix);
+    QFile file(Storage::transactionAccount().absoluteFilePath(fileName));
+    qDebug() << "Loading Transaction Account:" << value;
+
+    if(!file.open(QFile::ReadOnly))
+    {
+        qDebug() << "Loading Failed: Failed to open file.";
+        return TransactionAccount();
+    }
+
+    QDataStream stream(&file);
+    TransactionAccount data;
+    stream >> data;
+
+    if(stream.status() != QDataStream::Ok)
+    {
+        qDebug() << "Loading Failed: Failed to read file";
+        return TransactionAccount();
+    }
+    else
+    {
+        qDebug() << "Loading Completed.";
+        return data;
+    }
+}
+
+void TransactionAccount::saveToRecord(quint64 value) const
+{
+    if(!Identifier::isValid(value, Account::Transaction))
+    {
+        qDebug() << QString("The given ID (%1) is not valid for a transaction account").arg(value);
+        return;
+    }
+
+    const QString fileName = QString("%1.%2").arg(value).arg(suffix);
+    QFile file(Storage::transactionAccount().absoluteFilePath(fileName));
+    qDebug() << "Saving Transaction Account: " << value;
+
+    if(!file.open(QFile::WriteOnly))
+    {
+        qDebug() << "Saving Failed: Failed to open file.";
+        return;
+    }
+
+    QDataStream stream(&file);
+    stream << *this;
+    if(stream.status() != QDataStream::Ok)
+    {
+        qDebug() << "Saving Failed: Failed to write file";
+    }
+    else
+    {
+        qDebug() << "Saving Completed.";
+    }
+}
