2180db0bef003d156d8d764b74601cee667737fe
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-17 17:57:46 +0330

    Implemented abstract account
    This account provides a general implementation for an account.

---
+++ include/accounts/abstractaccount.h
@@ -0,0 +1,59 @@
+#ifndef ABSTRACTACCOUNT_H
+#define ABSTRACTACCOUNT_H
+
+#include "include/general.h"
+
+class AbstractAccount
+{
+public:
+    AbstractAccount();
+
+    quint16 getCvv2() const;
+    void setCvv2(quint16 cvv);
+    
+    quint64 getBalance() const;
+    void setBalance(quint64 newBalance);
+
+    quint64 getOwner() const;
+
+    quint64 getCardNumber() const;
+    void setCardNumber(quint64 number);
+
+    quint64 getShabaNumber() const;
+    void setShabaNumber(quint64 number);
+
+    QDate getExpirationDate() const;
+    void setExpirationDate(const QDate &date);
+
+    int getPrimaryPassword() const;
+    void setPrimaryPassword(int password);
+
+    int getSecondaryPassword() const;
+    void setSecondaryPassword(int password);
+
+    QDateTime getLastCashMoved() const;
+    void setLastCashMoved(const QDateTime &dateTime);
+
+    friend QDataStream& operator>>(QDataStream &stream, AbstractAccount &data);
+    friend QDataStream& operator<<(QDataStream &stream, const AbstractAccount &data);
+
+public:
+    virtual float profit() const = 0;
+    virtual void saveToRecord(quint64 value) const = 0;
+
+protected:
+    int secondaryPassword = 0;
+    QDateTime lastCashMoved;
+    int primaryPassword = 0;
+    quint64 cardNumber = 0;
+    quint64 shabaNumber = 0;
+    QDate expirationDate;
+    quint64 balance = 0;
+    quint16 cvv2 = 0;
+    quint64 owner = 0;
+};
+
+QDataStream& operator>>(QDataStream &stream, AbstractAccount &data);
+QDataStream& operator<<(QDataStream &stream, const AbstractAccount &data);
+
+#endif // ABSTRACTACCOUNT_H
---
+++ src/accounts/abstractaccount.cpp
@@ -0,0 +1,106 @@
+#include "include/accounts/abstractaccount.h"
+
+AbstractAccount::AbstractAccount() {}
+
+quint64 AbstractAccount::getBalance() const
+{
+    return balance;
+}
+
+void AbstractAccount::setBalance(quint64 newBalance)
+{
+    balance = newBalance;
+}
+
+QDate AbstractAccount::getExpirationDate() const
+{
+    return expirationDate;
+}
+
+void AbstractAccount::setExpirationDate(const QDate &date)
+{
+    expirationDate = date;
+}
+
+quint64 AbstractAccount::getCardNumber() const
+{
+    return cardNumber;
+}
+
+void AbstractAccount::setCardNumber(quint64 number)
+{
+    cardNumber = number;
+}
+
+quint64 AbstractAccount::getShabaNumber() const
+{
+    return shabaNumber;
+}
+
+void AbstractAccount::setShabaNumber(quint64 number)
+{
+    shabaNumber = number;
+}
+
+quint16 AbstractAccount::getCvv2() const
+{
+    return cvv2;
+}
+
+void AbstractAccount::setCvv2(quint16 cvv)
+{
+    cvv2 = cvv;
+}
+
+QDateTime AbstractAccount::getLastCashMoved() const
+{
+    return lastCashMoved;
+}
+
+void AbstractAccount::setLastCashMoved(const QDateTime &dateTime)
+{
+    lastCashMoved = dateTime;
+}
+
+quint64 AbstractAccount::getOwner() const
+{
+    return owner;
+}
+
+int AbstractAccount::getSecondaryPassword() const
+{
+    return secondaryPassword;
+}
+
+void AbstractAccount::setSecondaryPassword(int password)
+{
+    secondaryPassword = password;
+}
+
+int AbstractAccount::getPrimaryPassword() const
+{
+    return primaryPassword;
+}
+
+void AbstractAccount::setPrimaryPassword(int password)
+{
+    primaryPassword = password;
+}
+
+QDataStream& operator<<(QDataStream &stream, const AbstractAccount &data)
+{
+    stream << data.cardNumber << data.shabaNumber;
+    stream << data.balance << data.cvv2 << data.owner;
+    stream << data.expirationDate << data.lastCashMoved;
+    stream << data.primaryPassword << data.secondaryPassword;
+    return stream;
+}
+
+QDataStream& operator>>(QDataStream &stream, AbstractAccount &data)
+{
+    stream >> data.cardNumber >> data.shabaNumber;
+    stream >> data.balance >> data.cvv2 >> data.owner;
+    stream >> data.expirationDate >> data.lastCashMoved;
+    stream >> data.primaryPassword >> data.secondaryPassword;
+    return stream;
+}
