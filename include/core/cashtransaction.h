c40a2bcc011c2e24ae9b839c4229b467bf44b8c5
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-18 14:13:11 +0330

    Implemented a cash transaction machine
    This machine provides you with the ability to transfer cash.

--- include/core/cashtransaction.h
+++ include/core/cashtransaction.h
@@ -2,14 +2,51 @@
 #define CASHTRANSACTION_H
 
 #include <QObject>
+#include "include/general.h"
 
 class CashTransaction : public QObject
 {
     Q_OBJECT
 public:
+    Q_DISABLE_COPY_MOVE(CashTransaction)
     explicit CashTransaction(QObject *parent = nullptr);
+    long int getDynamicPassword() const;
+    Key getDestinationId() const;
+    long int getPassword() const;
+    double getAmount() const;
+    long int getCvv2() const;
+    Key getSourceId() const;
+
+protected:
+    virtual double income() const;
+    virtual double maximumTransaction() const;
+    virtual double maximumDailyTransaction() const;
+    virtual double maximumTransactionWithoutDynamicPassword() const;
+
+public slots:
+    void transfer();
+    void setCvv2(long int value);
+    void setAmount(double value);
+    void generateDynamicPassword();
+    void setPassword(long int value);
+    void setSourceId(const Key &value);
+    void setDestinationId(const Key &value);
 
 signals:
+    void destinationIdChanged(const Key &value);
+    void sourceIdChanged(const Key &value);
+    void passwordChanged(long int value);
+    void amountChanged(double value);
+    void cvv2Changed(long int value);
+    void dynamicPasswordCreated();
+    
+private:
+    long int dynamicPassword = 0;
+    Key destinationId = 0;
+    long int password = 0;
+    double amount = 0.0;
+    long int cvv2 = 0;
+    Key sourceId = 0;
 };
 
 #endif // CASHTRANSACTION_H
---
+++ src/core/cashtransaction.cpp
@@ -0,0 +1,161 @@
+#include "include/core/cashtransaction.h"
+#include "include/core/errors.h"
+#include "include/account.h"
+#include <QRandomGenerator>
+
+CashTransaction::CashTransaction(QObject *parent) : QObject{parent}
+{}
+
+double CashTransaction::maximumTransaction() const
+{
+    return 3 * std::pow(10, 6);
+}
+
+double CashTransaction::maximumDailyTransaction() const
+{
+    return 6 * std::pow(10, 6);
+}
+
+double CashTransaction::maximumTransactionWithoutDynamicPassword() const
+{
+    return 1 * std::pow(10, 5);
+}
+
+long int CashTransaction::getDynamicPassword() const
+{
+    return dynamicPassword;
+}
+
+void CashTransaction::generateDynamicPassword()
+{
+    dynamicPassword = QRandomGenerator::global()->bounded(quint64(0), quint64(std::pow(10, 5) - 1));
+    emit dynamicPasswordCreated();
+}
+
+Key CashTransaction::getDestinationId() const
+{
+    return destinationId;
+}
+
+void CashTransaction::setDestinationId(const Key &value)
+{
+    if (destinationId != value) {
+        destinationId = value;
+        emit destinationIdChanged(value);
+    }
+}
+
+Key CashTransaction::getSourceId() const
+{
+    return sourceId;
+}
+
+double CashTransaction::income() const
+{
+    return 0.01 / 100;
+}
+
+void CashTransaction::setSourceId(const Key &value)
+{
+    if (sourceId != value) {
+        sourceId = value;
+        emit sourceIdChanged(value);
+    }
+}
+
+long int CashTransaction::getPassword() const
+{
+    return password;
+}
+
+void CashTransaction::setPassword(long int value)
+{
+    if (password != value) {
+        password = value;
+        emit passwordChanged(value);
+    }
+}
+
+double CashTransaction::getAmount() const
+{
+    return amount;
+}
+
+void CashTransaction::setAmount(double value)
+{
+    if (amount != value) {
+        amount = value;
+        emit amountChanged(value);
+    }
+}
+
+long int CashTransaction::getCvv2() const
+{
+    return cvv2;
+}
+
+void CashTransaction::setCvv2(long int value)
+{
+    if (cvv2 != value) {
+        cvv2 = value;
+        emit cvv2Changed(value);
+    }
+}
+
+void CashTransaction::transfer()
+{
+    auto source = loadFromKey(sourceId);
+    if(source == nullptr)
+    {
+        throw UnknownSourceException();
+    }
+
+    auto destination = loadFromKey(destinationId);
+    if(destination == nullptr)
+    {
+        throw UnknownDestinationException();
+    }
+
+    if(QDate::currentDate() > source->getExpirationDate())
+    {
+        throw ExpiredCardException();
+    }
+
+    if(this->amount > this->maximumTransaction())
+    {
+        throw ExceededCashWithdraw();
+    }
+
+    auto final = (1 + this->income()) * this->amount;
+    if(final > source->getBalance())
+    {
+        throw NotEnoughBalanceException();
+    }
+
+    auto transferred = source->getTransferredBalance();
+    if(final + transferred > this->maximumDailyTransaction())
+    {
+        throw ExceededDailyTransactionLimit();
+    }
+    auto base = double {};
+    if(this->amount > maximumTransactionWithoutDynamicPassword())
+    {
+        base = dynamicPassword;
+    }
+    else
+    {
+        base = source->getSecondaryPassword();
+    }
+
+    if(base != password)
+    {
+        throw IncorrectPasswordException();
+    }
+
+    source->setTransferredBalance(source->getTransferredBalance() + this->amount);
+    destination->setBalance(destination->getBalance() + this->amount);
+    source->setBalance(source->getBalance() - final);
+
+    destination->saveToRecord(destinationId);
+    source->saveToRecord(sourceId);
+}
