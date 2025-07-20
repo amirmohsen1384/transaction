b2950647778a282f2e2d5905dc07cfffe7f356f4
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-21 00:00:14 +0330

    Implemented a password editor

---
+++ include/elements/passwordedit.h
@@ -0,0 +1,37 @@
+#ifndef PASSWORDEDIT_H
+#define PASSWORDEDIT_H
+
+#include <QLineEdit>
+#include <QPushButton>
+#include <QHBoxLayout>
+
+class PasswordEdit : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit PasswordEdit(QWidget *parent = nullptr);
+
+    bool hasFrame() const;
+    bool isReadOnly() const;
+    QString password() const;
+
+public slots:
+    void setFrame(bool value);
+    void setReadOnly(bool value);
+    void setPassword(const QString &value);
+
+private slots:
+    void updateIcon();
+    void togglePasswordMode(bool toggle);
+
+signals:
+    void passwordEdited(const QString &value);
+    void passwordChanged(const QString &value);
+
+private:
+    QLineEdit *editor = nullptr;
+    QHBoxLayout *layout = nullptr;
+    QPushButton *toggleButton = nullptr;
+};
+
+#endif // PASSWORDEDIT_H
---
+++ src/elements/passwordedit.cpp
@@ -0,0 +1,70 @@
+#include "include/elements/passwordedit.h"
+
+PasswordEdit::PasswordEdit(QWidget *parent) : QWidget(parent)
+{
+    layout = new QHBoxLayout(this);
+
+    editor = new QLineEdit(this);
+    editor->setClearButtonEnabled(true);
+    editor->setEchoMode(QLineEdit::Password);
+    connect(editor, &QLineEdit::textEdited, this, &PasswordEdit::passwordEdited);
+    connect(editor, &QLineEdit::textChanged, this, &PasswordEdit::passwordChanged);
+
+    toggleButton = new QPushButton(this);
+    connect(toggleButton, &QPushButton::toggled, this, &PasswordEdit::togglePasswordMode);
+    toggleButton->setCheckable(true);
+    toggleButton->setChecked(true);
+    toggleButton->setFlat(true);
+
+    layout->addWidget(editor);
+    layout->addWidget(toggleButton);
+    updateIcon();
+}
+
+bool PasswordEdit::hasFrame() const
+{
+    return editor->hasFrame();
+}
+
+bool PasswordEdit::isReadOnly() const
+{
+    return editor->isReadOnly();
+}
+
+QString PasswordEdit::password() const
+{
+    return editor->text();
+}
+
+void PasswordEdit::setFrame(bool value)
+{
+    editor->setFrame(value);
+}
+
+void PasswordEdit::setReadOnly(bool value)
+{
+    editor->setReadOnly(value);
+}
+
+void PasswordEdit::setPassword(const QString &value)
+{
+    editor->setText(value);
+}
+
+void PasswordEdit::updateIcon()
+{
+    if(editor->echoMode() == QLineEdit::Normal)
+    {
+        toggleButton->setIcon(QIcon(":/images/widgets/hide-password.png"));
+    }
+    else
+    {
+        toggleButton->setIcon(QIcon(":/images/widgets/show-password.png"));
+    }
+}
+
+void PasswordEdit::togglePasswordMode(bool toggle)
+{
+    editor->setEchoMode(toggle ? QLineEdit::Password : QLineEdit::Normal);
+    updateIcon();
+}
