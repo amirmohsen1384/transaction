504bd34ec565fe705c0e608861400e4ba396c4c9
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-21 10:12:30 +0330

    Fixed a bug in loading an account

--- src/account.cpp
+++ src/account.cpp
@@ -7,30 +7,46 @@ std::shared_ptr<AbstractAccount> loadFromKey(const Key &key)
     case Account::Loan:
     {
         auto account = std::make_shared<LoanAccount>();
-        if(Storage::loanAccount().exists(QString::number(key)))
+        if(Storage::loanAccount().exists(LoanAccount::getFilename(key)))
         {
             *account = LoanAccount::loadFromRecord(key);
+            qDebug() << "Successful to load loan account";
             return account;
+        }
+        else
+        {
+            qDebug() << "The given key which was a loan account is not available.";
         }
     }
     case Account::Deposit:
     {
         auto account = std::make_shared<DepositAccount>();
-        if(Storage::depositAccount().exists(QString::number(key)))
+        if(Storage::depositAccount().exists(DepositAccount::getFilename(key)))
         {
             *account = DepositAccount::loadFromRecord(key);
+            qDebug() << "Successful to load deposit account";
             return account;
+        }
+        else
+        {
+            qDebug() << "The given key which was a deposit account is not available.";
         }
     }
     case Account::Transaction:
     {
         auto account = std::make_shared<TransactionAccount>();
-        if(Storage::transactionAccount().exists(QString::number(key)))
+        if(Storage::transactionAccount().exists(TransactionAccount::getFilename(key)))
         {
             *account = TransactionAccount::loadFromRecord(key);
+            qDebug() << "Successful to load transaction account";
             return account;
+        }
+        else
+        {
+            qDebug() << "The given key which was a transaction account is not available.";
         }
     }
     }
+    qDebug() << QString("Failed to load account with %1. It was corrupt nor invalid").arg(key);
     return nullptr;
 }
