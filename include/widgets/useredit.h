e6e67e2445fd47d0d618fdae1d364a1e5521fb27
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-21 20:42:57 +0330

    Implemented the user editor

--- include/widgets/useredit.h
+++ include/widgets/useredit.h
@@ -3,6 +3,15 @@
 
 #include <QDialog>
 
+/*
+    First Name,
+    Last Name,
+    National Code,
+    User Name,
+    Password
+    Age
+*/
+
 namespace Ui {
 class UserEdit;
 }
@@ -15,8 +24,31 @@ public:
     explicit UserEdit(QWidget *parent = nullptr);
     ~UserEdit();
 
+    QString getNationalCode() const;
+    QString getFirstName() const;
+    QString getLastName() const;
+    QString getUserName() const;
+    QString getPassword() const;
+    int getAge() const;
+
+public slots:
+    void setNationalCode(const QString &value);
+    void setFirstName(const QString &value);
+    void setLastName(const QString &value);
+    void setUserName(const QString &value);
+    void setPassword(const QString &value);
+    void setAge(int value);
+
+signals:
+    void nationalCodeChanged(const QString &value);
+    void firstNameChanged(const QString &value);
+    void lastNameChanged(const QString &value);
+    void userNameChanged(const QString &value);
+    void passwordChanged(const QString &value);
+    void ageChanged(int value);
+
 private:
-    Ui::UserEdit *ui;
+    std::unique_ptr<Ui::UserEdit> ui;
 };
 
 #endif // USEREDIT_H
--- src/widgets/useredit.cpp
+++ src/widgets/useredit.cpp
@@ -1,9 +1,76 @@
 #include "include/widgets/useredit.h"
 #include "ui_useredit.h"
 
-UserEdit::UserEdit(QWidget *parent) : QDialog(parent), ui(new Ui::UserEdit)
+UserEdit::UserEdit(QWidget *parent) : QDialog(parent)
 {
+    ui = std::make_unique<Ui::UserEdit>();
     ui->setupUi(this);
+}
+
+QString UserEdit::getFirstName() const
+{
+    return ui->firstNameEdit->text();
+}
+
+QString UserEdit::getLastName() const
+{
+    return ui->lastNameEdit->text();
+}
+
+QString UserEdit::getNationalCode() const
+{
+    return ui->nationalCodeEdit->text();
+}
+
+QString UserEdit::getUserName() const
+{
+    return ui->userNameEdit->text();
+}
+
+QString UserEdit::getPassword() const
+{
+    return ui->passwordEdit->text();
+}
+
+int UserEdit::getAge() const
+{
+    return ui->ageEdit->value();
+}
+
+void UserEdit::setFirstName(const QString &value)
+{
+    ui->firstNameEdit->setText(value);
+    emit firstNameChanged(value);
+}
+
+void UserEdit::setLastName(const QString &value)
+{
+    ui->lastNameEdit->setText(value);
+    emit lastNameChanged(value);
+}
+
+void UserEdit::setNationalCode(const QString &value)
+{
+    ui->nationalCodeEdit->setText(value);
+    emit nationalCodeChanged(value);
+}
+
+void UserEdit::setUserName(const QString &value)
+{
+    ui->userNameEdit->setText(value);
+    emit userNameChanged(value);
+}
+
+void UserEdit::setPassword(const QString &value)
+{
+    ui->passwordEdit->setText(value);
+    emit passwordChanged(value);
+}
+
+void UserEdit::setAge(int value)
+{
+    ui->ageEdit->setValue(value);
+    emit ageChanged(value);
 }
 
 UserEdit::~UserEdit()
