e1aa3e81a8c6f26c46cccc8264ad88038a95f870
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-21 08:27:52 +0330

    Completed the account view
    This account is used to show the information of an account.

--- include/widgets/accountview.h
+++ include/widgets/accountview.h
@@ -1,6 +1,7 @@
 #ifndef ACCOUNTVIEW_H
 #define ACCOUNTVIEW_H
 
+#include "include/general.h"
 #include <QDialog>
 
 namespace Ui {
@@ -10,12 +11,24 @@ class AccountView;
 class AccountView : public QDialog
 {
     Q_OBJECT
+private:
+    void loadAccount(const Key &value);
 
 public:
+    explicit AccountView(const Key &number, QWidget *parent = nullptr);
     explicit AccountView(QWidget *parent = nullptr);
     ~AccountView();
 
+    Key getKey() const;
+
+public slots:
+    void setKey(const Key &value);
+
+signals:
+    void keyChanged(const Key &value);
+
 private:
+    Key value;
     Ui::AccountView *ui;
 };
 
--- src/widgets/accountview.cpp
+++ src/widgets/accountview.cpp
@@ -1,4 +1,5 @@
-#include "accountview.h"
+#include "include/widgets/accountview.h"
+#include "include/account.h"
 #include "ui_accountview.h"
 
 AccountView::AccountView(QWidget *parent) : QDialog(parent), ui(new Ui::AccountView)
@@ -11,4 +12,67 @@ AccountView::AccountView(QWidget *parent) : QDialog(parent), ui(new Ui::AccountV
 AccountView::~AccountView()
 {
     delete ui;
+}
+
+Key AccountView::getKey() const
+{
+    return value;
+}
+
+void AccountView::setKey(const Key &value)
+{
+    this->value = value;
+    emit keyChanged(value);
+}
+
+void AccountView::loadAccount(const Key &value)
+{
+    auto account = loadFromKey(value);
+    if(account == nullptr)
+    {
+        return;
+    }
+    switch(Identifier::accountMode(value))
+    {
+    case Account::Deposit:
+    {
+        ui->accountTypeLabel->setText("Deposit Account");
+        ui->accountTypeIcon->setPixmap(QPixmap(":/images/accounts/deposit-account.png"));
+        break;
+    }
+    case Account::Loan:
+    {
+        ui->accountTypeLabel->setText("Loan Account");
+        ui->accountTypeIcon->setPixmap(QPixmap(":/images/accounts/loan-account.png"));
+        break;
+    }
+    case Account::Transaction:
+    {
+        ui->accountTypeLabel->setText("Transaction Account");
+        ui->accountTypeIcon->setPixmap(QPixmap(":/images/accounts/transaction-account.png"));
+        break;
+    }
+    default:
+    {
+        ui->accountTypeLabel->setText("Unknown Account");
+        ui->accountTypeIcon->setPixmap(QPixmap());
+        break;
+    }
+    }
+    ui->cardNumberLabel->setText(account->getCardNumber());
+    ui->primaryPasswordView->setText(QString::number(account->getPrimaryPassword()));
+    ui->secondaryPasswordView->setText(QString::number(account->getSecondaryPassword()));
+    ui->cvv2Label->setText(QString::number(account->getCvv2()));
+    ui->balanceLabel->setText(QString::number(account->getBalance(), 'f', 2));
+    ui->expirationDateLabel->setText(account->getExpirationDate().toString(Qt::ISODate));
+    ui->transferredBalanceLabel->setText(QString::number(account->getTransferredBalance(), 'f', 2));
+    ui->shabaNumberLabel->setText(account->getShabaNumber());
+    ui->lastTransactionDateLabel->setText(account->getLastTransactionDate().toString(Qt::ISODate));
+    ui->shabaNumberLabel->setText(account->getShabaNumber());
+    ui->accountNumberLabel->setText(QString::number(value));
+    
+}
+
+AccountView::AccountView(const Key &number, QWidget *parent) : AccountView(parent)
+{
 }
