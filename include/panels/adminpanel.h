f86bb8126b8c28d63de690ed0811e6892f0c4b09
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-26 13:24:39 +0330

    Defined some functionality for admin panel

--- include/panels/adminpanel.h
+++ include/panels/adminpanel.h
@@ -2,6 +2,7 @@
 #define ADMINPANEL_H
 
 #include <QMainWindow>
+#include "include/general.h"
 
 namespace Ui {
 class AdminPanel;
@@ -10,12 +11,20 @@ class AdminPanel;
 class AdminPanel : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    explicit AdminPanel(QWidget *parent = nullptr);
+    explicit AdminPanel(const Key &value, QWidget *parent = nullptr);
     ~AdminPanel();
 
+    Key getIdentifier() const;
+
+public:
+    void setIdentifier(const Key &value);
+
+signals:
+    void identifierChanged(const Key &value);
+
 private:
+    Key identifier;
     std::unique_ptr<Ui::AdminPanel> ui;
 };
 
--- src/panels/adminpanel.cpp
+++ src/panels/adminpanel.cpp
@@ -1,10 +1,23 @@
 #include "include/panels/adminpanel.h"
 #include "ui_adminpanel.h"
 
-AdminPanel::AdminPanel(QWidget *parent) : QMainWindow(parent)
+AdminPanel::AdminPanel(const Key &value, QWidget *parent) : QMainWindow(parent)
 {
     ui = std::make_unique<Ui::AdminPanel>();
     ui->setupUi(this);
+
+    setIdentifier(value);
 }
 
 AdminPanel::~AdminPanel() {}
+
+Key AdminPanel::getIdentifier() const
+{
+    return identifier;
+}
+
+void AdminPanel::setIdentifier(const Key &value)
+{
+    identifier = value;
+    emit identifierChanged(value);
+}
