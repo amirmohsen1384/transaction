7b96e8a8578ea3708c828399c889fe5e0ef4e769
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-26 10:38:44 +0330

    Defined the main point

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <QApplication>
+#include "include/panels/welcomepanel.h"
+#include "include/panels/customerpanel.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+
+    std::unique_ptr<QMainWindow> window;
+
+    WelcomePanel panel;
+    panel.open();
+
+    connect(&panel, &WelcomePanel::adminLoggedIn, this, [&](const Key &id)
+        {
+
+        }
+    );
+    connect(&panel, &WelcomePanel::customerLoggedIn, this, [&](const Key &id)
+        {
+            window = std::make_unique<CustomerPanel>(id);
+            window->show();
+        }
+    );
 
     return app.exec();
 }
