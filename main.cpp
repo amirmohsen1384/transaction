1076c3b745826c3518e0b00cf298126c89b338e4
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-07-26 11:32:34 +0330

    Implemented a new feature
    This feature reset transferred balance after 24 hours from the last transaction.

--- main.cpp
+++ main.cpp
@@ -8,12 +8,59 @@ int main(int argc, char **argv)
 
     std::unique_ptr<QMainWindow> window;
 
+    const auto &depositList = Storage::depositAccount().entryInfoList
+    (
+        {QString("*.%1").arg(deposit_suffix)},
+        QDir::AllEntries | QDir::NoDotAndDotDot
+    );
+    for(const auto &account : depositList)
+    {
+        const auto id = account.baseName().toLongLong();
+        auto data = DepositAccount::loadFromRecord(id);
+        if(data.getLastCashMoved().addSecs(60 * 60 * 24) < QDateTime::currentDateTime())
+        {
+            data.setTransferredBalance(0);
+            qDebug() << QString("Transferred balance of %1 with %2 has been reset.").arg(id).arg(data.getLastCashMoved().toString());
+        }
+        data.saveToRecord(id);
+    }
+
+    const auto &loanList = Storage::loanAccount().entryInfoList
+    (
+        {QString("*.%1").arg(loan_suffix)},
+        QDir::AllEntries | QDir::NoDotAndDotDot
+    );
+    for(const auto &account : loanList)
+    {
+        const auto id = account.baseName().toLongLong();
+        auto data = LoanAccount::loadFromRecord(id);
+        if(data.getLastCashMoved().addSecs(60 * 60 * 24) < QDateTime::currentDateTime())
+        {
+            data.setTransferredBalance(0);
+            qDebug() << QString("Transferred balance of %1 with %2 has been reset.").arg(id).arg(data.getLastCashMoved().toString());
+        }
+        data.saveToRecord(id);
+    }
+
+    const auto &transactionList = Storage::transactionAccount().entryInfoList
+    (
+        {QString("*.%1").arg(transaction_suffix)},
+        QDir::AllEntries | QDir::NoDotAndDotDot
+    );
+    for(const auto &account : transactionList)
+    {
+        const auto id = account.baseName().toLongLong();
+        auto data = TransactionAccount::loadFromRecord(id);
+        if(data.getLastCashMoved().addSecs(60 * 60 * 24) < QDateTime::currentDateTime())
+        {
+            data.setTransferredBalance(0);
+            qDebug() << QString("Transferred balance of %1 with %2 has been reset.").arg(id).arg(data.getLastCashMoved().toString());
+        }
+        data.saveToRecord(id);
+    }
+
     WelcomePanel panel;
     panel.open();
-
-    Customer cs = Customer::loadFromRecord(2133788);
-    qDebug() << cs.getUserName();
-    qDebug() << cs.getPassword();
 
     QObject::connect(&panel, &WelcomePanel::adminLoggedIn, [&](const Key &id)
         {
